b506cd8ad425167003b8e1f09d29b1bae044adf6
amitkumar <realamit678@gmail.com>
2022-04-05 13:45:32 +0530

    string

---
+++ script.js
@@ -0,0 +1,80 @@
+// let mystr='asdfghjklzxcvbnm,qwertyui'
+// let result=mystr.charAt(5)
+// console.log(result);
+//if we want to find the charectar of srtring then use charAt method
+// const str1='hello prepbytes'
+// const string1=str1.indexOf('o')
+// console.log(string1);
+
+//  let mystr='asdfghjklzxcvbnm,qwertyui'
+//  let result=mystr.indexOf('s')
+//  console.log(result);
+
+ //if we want to find the index then use the indexof  here also count the space
+
+ //split: splite method is used to convert the string to array splite method return a new array does not change the original value
+//  var text ='amit'
+//  let result=text.split('')
+//  console.log(result);
+// const str ='helo my name is amit'
+// const string=str.length
+// console.log(string);
+
+// split
+// const str1='how are you today'
+// const string=str1.split('')
+// console.log(string);
+// console.log(str1);
+
+// const str='hello amit'
+// const string=str.split("").reverse().join("")
+// console.log(string);
+
+// const str='hello amit'
+// string=str.split("").reverse().join("")
+// console.log(string);
+
+
+//trim
+
+        //  const str="               hello                    "
+        //  console.log(str.length);
+        // const string=str.trim()
+       
+        // console.log(string);
+        // console.log(string.length);
+
+
+        //SLICE
+        // const str='hello,everyone what are,doing'
+        // const string=str.slice(0,-5)
+        // console.log(string);
+
+        //concate 
+//         const str='hello'
+//     const str1='everyone'
+//     const string=str.concat(str1)
+// console.log(string);
+
+
+
+//repelace
+// const str ="welcome amit kumar"
+// const string=str.replace("welcome","go back")
+// console.log(string);
+
+// const str='hello amit'
+// const string=str.split("").join("")
+// console.log(string);
+
+// const str="     amit         "
+// console.log(str.length);
+// const string=str.trim()
+// console.log(string.length);
+
+
+// const str="amit kumar"
+
+// const string=str.split("").reverse("").join("")
+// console.log(string);
+
